f19db22b8d8083c5703920e52a59b6964ae1e4cf
Himanshu Chaudhary <himanshu266@gmail.com>
2013-11-06 12:04:10 -0500

    Some refactoring

--- js/app.js
+++ js/app.js
@@ -5,16 +5,16 @@ var app = (function() {
 	return {
 		/* Initialize App */
 		start: function() {
-			/* Enable Hammer JS */
+			// Enable Hammer JS
 			hammer = $(document.body).hammer();
 
-			/* Menu Link Tap */
+			// Menu Link Tap
 			hammer.on('tap', 'aside li[data-target]', function() {
 				app.hideAside();
 				app.section(this.getAttribute('data-target'), 'aside');
 			});
 
-			/* Section Link Tap */
+			// Section Link Tap
 			hammer.on('tap', 'section li[data-target]', function() {
 				/* t:target, a:animation, h:header */
 				var t = this.getAttribute('data-target');
@@ -32,7 +32,7 @@ var app = (function() {
 				}
 			});
 
-			/* Group List Tap */
+			// Group List Tap
 			hammer.on('tap', 'ul.group li', function() {
 				this.parentNode.querySelector('li.active').classList.remove('active');
 				this.classList.add('active');
@@ -56,30 +56,30 @@ var app = (function() {
 			var nw = document.getElementById('notificationWrapper');
 			var nb = document.getElementById('notificationBody');
 
-			/* Show Notification */
+			// Show Notification
 			nw.className = 'show';
 			nb.className = 'show';
 
-			/* Set time to 1.5 sec if not provided */
+			// Set time to 1.5 sec if not provided
 			if (!data.time) { 
 				data.time = 1500; 
 			}
 			
-			/* Set Style (alert, error, success, info) */
+			// Set Style (alert, error, success, info)
 			nb.classList.add(data.style);
 
-			/* Set Icon */
+			// Set Icon
 			if (data.icon) { 
 				nb.querySelector('i').className = data.icon + ' show'; 
 			}
 
-			/* Set Title */
+			// Set Title
 			nb.querySelector('.title').innerHTML = data.title;
 
-			/* Set Message */
+			// Set Message
 			nb.querySelector('.message').innerHTML = data.message;
 			
-			/* Show Buttons */
+			// Show Buttons
 			if (data.buttons) {
 				nb.ontap = null;
 				nb.querySelector('.buttons').classList.add('show');
@@ -99,10 +99,10 @@ var app = (function() {
 				};
 			}
 			else {
-				/* Hide Notification On Tap */
+				// Hide Notification On Tap
 				nb.ontap = app.hideNotification;
 				
-				/* Hide Notification After Given Time */
+				// Hide Notification After Given Time
 				if (data.time !== '0') { 
 					notificationId = setTimeout(app.hideNotification, data.time); 
 				}
@@ -114,10 +114,10 @@ var app = (function() {
 			var nw = document.getElementById('notificationWrapper');
 			var nb = document.getElementById('notificationBody');
 
-			/* Clear Timeout if there is one */
+			// Clear Timeout if there is one
 			clearTimeout(notificationId);
 
-			/* Hide Notification Body, Icon, Buttons & Wrapper */
+			// Hide Notification Body, Icon, Buttons & Wrapper
 			nb.classList.add('hide');
 			setTimeout(function() { 
 				nw.classList.remove('show');
@@ -128,18 +128,18 @@ var app = (function() {
 
 		/* Navigate To Section (s:section, a:animation) */
 		section: function(s, a) {
-			/* cs: current section, ts: target section */ 
+			// cs: current section, ts: target section
 			var cs = document.querySelector('section.current');
 			var ts = document.getElementById(s);
 
-			/* Update Classes */
+			// Update Classes
 			function updateSection() {
-				/* Check if Target and Current same */
+				// Check if Target and Current same
 				if (cs !== ts) {			
 					ts.className = a + ' in current';
 					cs.className = a + ' out current';
 
-					/* Remove Animation Classes Once Done */
+					// Remove Animation Classes Once Done
 					ts.addEventListener('webkitAnimationEnd', function() {
 						ts.classList.remove(a);
 						ts.classList.remove('in');
@@ -153,14 +153,14 @@ var app = (function() {
 					});
 				}
 
-				/* Update Menu Active */
+				// Update Menu Active
 				document.querySelector('#asideMenu ul li.active').classList.remove('active');
 				document.querySelector('#asideMenu ul li[data-target="' + s + '"]').classList.add('active');
 			}
 			
-			/* Check If Aside Is Open */
+			// Check If Aside Is Open
 			if (cs.classList.contains('show-menu')) {
-				/* Go To Section After Aside Closed */
+				// Go To Section After Aside Closed
 				app.hideAside();
 				setTimeout(updateSection, 150);
 			}
@@ -182,8 +182,8 @@ var app = (function() {
 				ac.classList.remove('current'); 
 			}
 			if (fc = cs.querySelector('footer li.active')) { 
-				fc.classList.remove('active') 
-			};
+				fc.classList.remove('active');
+			}
 			if (ft = cs.querySelector('footer li[data-target="' + a + '"]')) { 
 				ft.classList.add('active'); 
 			}
